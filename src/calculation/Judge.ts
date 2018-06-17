9f5f1af915376be3f0b76ba1628896542cb59984
kasakaid <kasakaid@outlook.com>
2018-06-17 19:46:03 +0900

    now you discard hard cord of winning condition

--- src/calculation/Judge.ts
+++ src/calculation/Judge.ts
@@ -1,25 +1,53 @@
 class Judge {
 
-    public calculateWinner(squares: Array<string | null>) {
-        const winnings = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ];
-        for (const winning of winnings) {
-          if (squares[winning[0]] &&
-            squares[winning[0]] === squares[winning[1]] &&
-            squares[winning[0]] === squares[winning[2]]) {
-            return squares[winning[0]];
-          }
-        }
-        return null;
+  public calculateWinner(squares: Array<string | null>) {
+
+    for (const winning of this.winningCondition()) {
+      if (squares[winning[0]] &&
+        squares[winning[0]] === squares[winning[1]] &&
+        squares[winning[0]] === squares[winning[2]]) {
+        return squares[winning[0]];
+      }
     }
+    return null;
+  }
+  /**
+   * 勝利条件を生成します。
+   * 縦、横、斜めでかつ条件を生成します。
+   */
+  private winningCondition(): number[][] {
+    const horizontal: number[][] = [];
+    const horizontalFn = (current: number): number[][] => {
+      if (current === 3) {
+        return horizontal;
+      }
+      let start = current;
+      horizontal.push([start, ++start, ++start]);
+      return horizontalFn(++current);
+    };
+    const vertical: number[][] = [];
+    const verticalFn = (current: number): number[][] => {
+      if (current === 3) {
+        return vertical;
+      }
+      vertical.push([current, current + 3, current + 6]);
+      return verticalFn(++current);
+    };
+    const slanting: number[][] = [];
+    const slantingFn = (current: number): number[][] => {
+      if (current === 4) {
+        return slanting;
+      }
+      const cornerSummary = 8;
+      const end = cornerSummary - current;
+      slanting.push([current, cornerSummary / 2, end]);
+      return slantingFn(current + 2);
+    };
+    return horizontalFn(0).concat(
+      verticalFn(0).concat(
+        slantingFn(0),
+      ));
+  }
 }
 
 export default Judge;
